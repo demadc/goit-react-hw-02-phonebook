d7b9ddf4e6c7ad1a14389c716589c755a4ab333c
Demian <demyan.korotchenko@gmail.com>
2023-12-26 23:45:05 +0100

    styled

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,15 @@ import { Component } from 'react';
 import { ContactForm } from './Form/Form';
 import { ContactsList } from './ContactList/ContactsList';
 import { Filter } from './Filter/Filter';
+import { Layout } from './Layout/Layout';
+import initialContacts from './contacts.json';
+import { GlobalStyle } from './GlobalStyle';
+import { Section } from './Section/Section';
+import { Header } from './Header/Header';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: initialContacts,
     filter: '',
   };
 
@@ -49,16 +49,20 @@ export class App extends Component {
     );
 
     return (
-      <div style={{ width: 500, marginLeft: 400, marginRight: 400 }}>
-        <h2>Phonebook</h2>
-        <ContactForm onAddContact={this.handleAddContact} />
-        <h2>Contacts</h2>
-        <Filter value={this.state.filter} onFilter={this.handleFilter} />
-        <ContactsList
-          contacts={itemContacts}
-          onDeleteContact={this.handleDeleteContact}
-        />
-      </div>
+      <Layout>
+        <Section title="Phonebook">
+          <ContactForm onAddContact={this.handleAddContact} />
+          <Header title="Contacts" />
+          <Filter value={this.state.filter} onFilter={this.handleFilter} />
+          <ContactsList
+            contacts={itemContacts}
+            onDeleteContact={this.handleDeleteContact}
+          />
+        </Section>
+
+        {/* <ToastContainer /> */}
+        <GlobalStyle />
+      </Layout>
     );
   }
 }
--- src/components/Form/Form.styled.jsx
+++ src/components/Form/Form.styled.jsx
@@ -2,6 +2,14 @@ import styled from 'styled-components';
 
 export const Form = styled.form`
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 12px;
+  margin-bottom: ${props => props.theme.space[4]}px;
+  padding: ${props => props.theme.space[3]}px;
+
+  max-width: 100%;
+  width: 500px;
 `;
 
 export const Label = styled.label`
@@ -13,24 +21,64 @@ export const Label = styled.label`
   width: 170px;
   margin-bottom: 32px;
 
-  color: 000;
+  color: ${props => props.theme.colors.accent};
   font-weight: 400;
   font-size: 16px;
 `;
 
 export const Field = styled.input`
-  margin-left: auto;
-  margin-right: auto;
-  padding: 5px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
 
-  font-size: 12px;
+  max-width: 100%;
+  width: 500px;
 
-  border: 1px solid rgba(33, 33, 33, 0.2);
-  border-radius: 12px;
+  color: ${props => props.theme.colors.accent};
+  text-shadow: ${props => props.theme.shadows.textShadow};
 `;
 
 export const Button = styled.button`
-  width: 90px;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  gap: ${p => p.theme.space[2]}px;
 
-  text-align: center;
+  padding-top: ${p => p.theme.space[3]}px;
+  padding-bottom: ${p => p.theme.space[3]}px;
+  padding-left: ${p => p.theme.space[4]}px;
+  padding-right: ${p => p.theme.space[4]}px;
+
+  max-width: 100%;
+
+  border: ${p => p.theme.borders.none};
+  outline: none;
+  border-radius: 8px;
+
+  box-shadow: ${p => p.theme.shadows.boxShadow};
+  transition: all 0.2s ease-in-out;
+
+  background-color: ${p => p.theme.colors.muted};
+  color: ${p => p.theme.colors.white};
+  cursor: pointer;
+
+  :hover,
+  :focus {
+    background-color: ${p => p.theme.colors.secondary};
+    color: ${p => p.theme.colors.accent};
+    box-shadow: -2px -2px 5px #fff, 2px 2px 5px #8ba793;
+    svg {
+      fill: ${p => p.theme.colors.accent};
+      stroke: ${p => p.theme.colors.accent};
+    }
+  }
+
+  :active {
+    color: ${p => p.theme.colors.accent};
+    box-shadow: inset 1px 1px 2px #8ba793, inset -1px -1px 2px #fff;
+    svg {
+      fill: ${p => p.theme.colors.accent};
+      stroke: ${p => p.theme.colors.accent};
+    }
+  }
 `;
